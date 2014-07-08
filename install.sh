e99b0dbd4f3ec47db3d47dc4a8e98cd01ee57de2
Adrien Folie <folie.adrien@gmail.com>
2014-07-08 14:56:43 +0000

    vim plugins

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ ln -s bashrc $HOME/.bashrc
 source $HOME/.bashrc
 
 # Vim config
+mkdir -p vim/tmp/backup
+mkdir -p vim/tmp/swap
+mkdir -p vim/tmp/undo
+
 ln -s vim $HOME/.vim
 ln -s vimrc $HOME/.vimrc
