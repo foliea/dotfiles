4e71b45e23c38d114542b0db7fe4b90e84c320fd
Adrien Folie <folie.adrien@gmail.com>
2014-07-08 12:06:13 +0200

    Update and rename gitconfig.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# Git config
 git config --global user.name "Adrien Folie"
 git config --global user.email "folie.adrien@gmail.com"
+
+# Github config
 git config --global github.user "folieadrien"
+
+# Bashrc
+ln -s .bashrc ~/.bashrc
+source ~/.bashrc
