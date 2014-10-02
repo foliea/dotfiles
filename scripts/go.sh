68aae33caf279b92b9bdf9f8123498b28e21c0c7
Adrien Folie <folie.adrien@gmail.com>
2014-10-02 12:21:26 +0200

    Update go.sh

--- scripts/go.sh
+++ scripts/go.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-if [ -z "$GOPATH" ]; then
-    mkdir -p $HOME/dev/go
-    export GOPATH=$HOME/dev/go
-    exec $SHELL -i
-    echo "GOPATH set"
+read -p "Please specify your $GOPATH: " gopath
+if [ -z "$gopath" ]; then
+    echo "$gopath" >> "$HOME"/.bashrc
+    source "$HOME"/.bashrc
 fi
 
-go get -u code.google.com/p/go.tools/cmd/oracle
-go get -u code.google.com/p/go.tools/cmd/goimports
-go get -u github.com/nsf/gocode
-go get -u github.com/golang/lint/golint
-go get -u github.com/kisielk/errcheck
-go get -u code.google.com/p/rog-go/exp/cmd/godef
-go get -u github.com/jstemmer/gotags
-
-ln -s "$GOPATH"/bin "$HOME"/.vim-go
+if [ -z "$GOPATH" ]; then
+    go get -u code.google.com/p/go.tools/cmd/oracle
+    go get -u code.google.com/p/go.tools/cmd/goimports
+    go get -u github.com/nsf/gocode
+    go get -u github.com/golang/lint/golint
+    go get -u github.com/kisielk/errcheck
+    go get -u code.google.com/p/rog-go/exp/cmd/godef
+    go get -u github.com/jstemmer/gotags
+    
+    ln -s "$GOPATH"/bin "$HOME"/.vim-go
+fi
