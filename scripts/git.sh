f45697021f00b201d762a4827b6682d704c6b761
Adrien Folie <folie.adrien@gmail.com>
2014-10-02 12:01:26 +0200

    Create git.sh

---
+++ scripts/git.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+read -p "Please specify you git user name: " username
+if [ "$username" ]; then
+    $(git config --global user.name "$username")
+fi
+
+read -p "Please specify you git user email: " useremail
+if [ "$useremail" ]; then
+    $(git config --global user.email "$useremail")
+fi
