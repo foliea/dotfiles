923acb3d06d0056faceaec57e5ab3a98a9815e08
Adrien Folie <folie.adrien@gmail.com>
2015-01-08 15:45:53 +0000

    add missing brew update

--- darwin/install.sh
+++ darwin/install.sh
@@ -6,6 +6,8 @@ if [ ! $(command -v brew) ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew update
+
 # Install dependencies
 brew install git \
              mercurial \
