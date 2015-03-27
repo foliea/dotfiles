e94b5596ff61d1e38b489fdeda5c66f318f56b8a
Adrien Folie <folie.adrien@gmail.com>
2015-03-27 01:26:09 +0100

    remove install apps from global script

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,14 +6,6 @@ echo "Warning! dependencies can be installed only on Darwin and Ubuntu"
 
 if [ `uname -s` = "Darwin" ]; then
     ./darwin/install.sh
-
-    # Install applications
-    read -p "Also install applications? (y/n) " choice
-    case "$choice" in
-      y|Y ) eval install_applications;;
-      n|N ) echo "Applications won't be installed.";;
-      * ) echo "Invalid choice. Aborting applications installation.";;
-    esac
 elif [ `uname -s` = "Linux" ]; then
     ./ubuntu/install.sh
 fi
