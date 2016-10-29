788f4313f59ef7438c7fb10f2c7a80f066d83482
Adrien Folie <folie.adrien@gmail.com>
2016-10-29 07:13:40 +0200

    replace rvm by rbenv

--- os/arch/applications.sh
+++ os/arch/applications.sh
@@ -3,11 +3,7 @@ set -e
 
 yaourt -Sy --noconfirm heroku-toolbelt \
                        atom-editor \
-<<<<<<< HEAD
                        google-chrome \
                        caffeine-ng
-=======
-                       caffeine-ng \
                        rbenv \
                        ruby-build
->>>>>>> remove rvm for rbenv
--- ruby/install.sh
+++ ruby/install.sh
@@ -3,16 +3,4 @@ set -e
 
 if [ ! $VERSION ]; then VERSION="2.1.5" ; fi
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-
-# Install Ruby Version Manager
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-# Source rvm
-source "$HOME/.rvm/scripts/rvm"
-
-# Install Ruby VERSION
-rvm install ruby-$VERSION
-
-# Use Ruby VERSION as default
-rvm --default use ruby-$VERSION
+rbenv install $VERSION
