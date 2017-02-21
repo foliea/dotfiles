3c2694cdf26ac457bc276ffb494d7862749331dc
Adrien Folie <foliea@users.noreply.github.com>
2017-02-21 16:40:32 +0100

    Update applications.sh

--- os/darwin/applications.sh
+++ os/darwin/applications.sh
@@ -21,8 +21,3 @@ brew cask install \
     virtualbox \
     android-file-transfer \
     docker
-
-# Enable tap to click (Trackpad) for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
