8d3925b188fdab4fc4e2815b2726d87c01ad01ed
Adrien Folie <folie.adrien@gmail.com>
2017-06-09 23:09:31 +0200

    tmux plugins fixes

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,3 +4,6 @@ set -e
 ln -s $PWD/tmux/tmux-colors.conf $HOME/.tmux-colors.conf
 ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/tmux $HOME/.tmux
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
