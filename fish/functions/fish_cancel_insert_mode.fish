b9e7af740c1d49b930e0b373e903720b41ccf290
Adrien Folie <folie.adrien@gmail.com>
2017-06-30 15:40:11 +0200

    Revert "use vi key bindings in fish"
    This reverts commit b269ac4277241be495b55f1a8396dee61ebf26d0.

--- fish/functions/fish_cancel_insert_mode.fish
+++
@@ -1,8 +0,0 @@
-function fish_cancel_insert_mode
-  if commandline -P
-    commandline -f cancel
-  else
-    set fish_bind_mode default
-    commandline -f backward-char force-repaint
-  end
-end
--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-function fish_user_key_bindings
-  fish_vi_key_bindings
-
-  bind -M insert -m default \co fish_cancel_insert_mode
-end
