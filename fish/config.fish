b66ff87b4c2d9f966dd0dc43b4526abe5e034a75
Adrien Folie <folie.adrien@gmail.com>
2017-08-12 01:22:08 +0000

    add vim indentline

--- fish/config.fish
+++ fish/config.fish
@@ -18,7 +18,7 @@ end
 # Tmux
 if not test $TMUX;
     if type tmux > /dev/null 2>&1
-        tmux has-session -t default; and tmux attach-session -t default; or tmux new-session -s default; and kill %self
+        tmux has-session -t default; and tmux attach-session -d -t default; or tmux new-session -s default; and kill %self
     end
 end
 
