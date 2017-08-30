904fdf62f3b45850cb33ad81df145b16b36dd429
Adrien Folie <folie.adrien@gmail.com>
2017-08-30 23:04:59 +0200

    remove yakyak

--- os/arch/applications.sh
+++ os/arch/applications.sh
@@ -5,7 +5,6 @@ yaourt -Sy --noconfirm heroku-toolbelt \
                        google-chrome \
                        vlc \
                        qt4 \ # Required by VLC
-                       yakyak \
                        albert \
                        rbenv \
                        ruby-build
