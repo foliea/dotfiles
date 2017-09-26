2a5323b6b6376ed65849aff209e8688686fad8bf
Adrien Folie <folie.adrien@gmail.com>
2017-09-26 16:51:03 +0200

    fix gpg agent

--- fish/config.fish
+++ fish/config.fish
@@ -15,7 +15,7 @@ if type rbenv > /dev/null 2>&1
 end
 
 # Verify that gpg-agent config exists
-if test -d $GNUPGHOME/gpg-agent.conf
+if test -d $GNUPGHOME
     set -g -x SSH_AGENT_PID
     set -g -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent.ssh
 end
