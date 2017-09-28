975cac1f62fa7d854c9ee96a5c3b2e29d3f7cef9
Adrien Folie <folie.adrien@gmail.com>
2017-09-28 20:58:43 +0200

    [arch] improve monitor switch

--- os/arch/i3/switch-monitor.sh
+++ os/arch/i3/switch-monitor.sh
@@ -5,27 +5,45 @@ monitors=($(xrandr | awk '( $2 == "connected" ){ print $1 }'))
 internal_output=${monitors[0]}
 external_output=${monitors[1]}
 
-# if we don't have a file, start at zero
-if [ ! -f "/tmp/monitor_mode.dat" ] ; then
-    monitor_mode="all"
-# otherwise read the value from the file
+current_monitor_mode=`cat /tmp/monitor_mode.dat 2>/dev/null`
+
+if [ -z "$1" ] ; then
+    without_notification=true
+
+    if [ -z "$current_monitor_mode" ] ; then
+        requested_mode="internal"
+    else
+        requested_mode=$current_monitor_mode
+    fi
 else
-    monitor_mode=`cat /tmp/monitor_mode.dat`
+    requested_mode="$1"
 fi
 
-if [ $monitor_mode = "all" ]; then
-    monitor_mode="external"
-    xrandr --output $internal_output --off --output $external_output --auto
-elif [ $monitor_mode = "external" ]; then
-    monitor_mode="internal"
-    xrandr --output $internal_output --auto --primary --output $external_output --off
+if [ $requested_mode = "cycle" ] ; then
+    # Cycle between internal and external
+    if [ $current_monitor_mode = "internal" ]; then
+        monitor_mode="external"
+    else
+        monitor_mode="internal"
+    fi
 else
-    monitor_mode="all"
-    xrandr --output $internal_output --auto --output $external_output --auto --left-of $internal_output
+    monitor_mode=$requested_mode
 fi
 
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
 
-notify-send "Switch monitor" "Selected mode: ${monitor_mode}"
+if [ $monitor_mode = "external" ]; then
+    xrandr --output $internal_output --off --output $external_output --auto --primary
+elif [ $monitor_mode = "internal" ]; then
+    xrandr --output $internal_output --auto --primary --output $external_output --off
+elif [ $monitor_mode = "clone" ]; then
+    xrandr --output $internal_output --auto --primary --output $external_output --auto --same-as $internal_output
+elif [ $monitor_mode = "extended" ]; then
+    xrandr --output $internal_output --auto --output $external_output --auto --primary --left-of $internal_output
+fi
 
-sh $HOME/.config/polybar/launch.sh
+if [ -z "$without_notification" ] ; then
+    notify-send "Switch monitor" "Selected mode: ${monitor_mode}"
+
+    sh $HOME/.config/polybar/launch.sh
+fi
