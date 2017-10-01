5716fefc6b04dd9c72e0f24a0c0b01dc35412b32
Adrien Folie <folie.adrien@gmail.com>
2017-10-01 04:26:44 +0200

    [arch] add initramfs splash screen

--- os/arch/razer-blade-stealth.sh
+++ os/arch/razer-blade-stealth.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-# Fix suspend loop issue
-sudo sed 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=quiet button.lid_init_state=open/' /etc/default/grub > /dev/null
+sudo pacman -S --noconfirm --needed plymouth
+
+sudo cp -f $PWD/os/arch/misc/mkinitcpio.conf /etc/mkinitcpio.conf
+
+sudo mkinitcpio -p linux413
+
+# Fix suspend loop issue and add plymouth splash
+sudo sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash button.lid_init_state=open"/' /etc/default/grub > /dev/null
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
