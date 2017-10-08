c73fb90ffe92e92eb1066462456d2c6c77dca417
Adrien Folie <folie.adrien@gmail.com>
2017-10-09 01:18:52 +0200

    [arch] remove symbolic link on transmission

--- os/arch/link.sh
+++ os/arch/link.sh
@@ -12,7 +12,6 @@ for file in i3 \
     terminator \
     dunst \
     gtk-3.0 \
-    transmission \
     compton.conf \
     clipit \
     qutebrowser
@@ -30,3 +29,10 @@ do
 done
 
 ln -s /run/media/$USER $HOME/Mount
+
+# Transmission is using the same config file to update its own
+# variables, making it painful to have linked to a file in source
+# control.
+cp -r $PWD/os/arch/transmission $config_dir/
+
+sed -i -e "s/{{USER}}/$USER/" $config_dir/transmission/settings.json 1>/dev/null
