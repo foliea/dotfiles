2da47de6ec7fd3b8d0363d32591db43141adff0a
Adrien Folie <folie.adrien@gmail.com>
2021-09-22 14:48:33 +0200

    feat(vim): improve tooling for js development

--- vim/install.sh
+++ vim/install.sh
@@ -14,3 +14,25 @@ ln -s $PWD/vim/tern-config $HOME/.tern-config
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install CoC extensions
+mkdir -p $HOME/.config/coc/extensions
+
+cd $HOME/.config/coc/extensions
+
+if [ ! -f package.json ]; then
+  echo '{"dependencies":{}}'> package.json
+fi
+
+for extension in coc-tsserver \
+    coc-json \
+    coc-prisma \
+    coc-sh \
+    coc-yaml \
+    coc-cmake \
+    coc-swagger
+do
+    npm install $extension --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+done
+
+cd -
