86f5cb85c0b56aa2018543ca9e31d4db09d5e7eb
Adrien Folie <folie.adrien@gmail.com>
2025-05-30 20:06:05 +0200

    chore: fix indentation on some files

--- os/macOS/dependencies.sh
+++ os/macOS/dependencies.sh
@@ -2,25 +2,25 @@
 set -e
 
 brew install cmake \
-        curl \
-        wget \
-        gawk \
-        readline \
-        most \
-        jq \
-        fish \
-        tmux \
-        macvim \
-        vifm \
-        ncdu \
-        fzf \
-        ripgrep \
-        hub \
-        rar \
-        rbenv \
-        p7zip \
-        kubectx \
-        kubeseal \
-        helm
+  curl \
+  wget \
+  gawk \
+  readline \
+  most \
+  jq \
+  fish \
+  tmux \
+  macvim \
+  vifm \
+  ncdu \
+  fzf \
+  ripgrep \
+  hub \
+  rar \
+  rbenv \
+  p7zip \
+  kubectx \
+  kubeseal \
+  helm
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
--- os/ubuntu/dependencies.sh
+++ os/ubuntu/dependencies.sh
@@ -4,35 +4,35 @@ set -e
 add-apt-repository -y ppa:longsleep/golang-backports
 apt-get update -qq
 apt-get install -qy \
-    build-essential \
-    cmake \
-    gawk \
-    git \
-    hub \
-    mercurial \
-    curl \
-    wget \
-    vim \
-    fish \
-    tmux \
-    rbenv \
-    libffi-dev \
-    libyaml-dev \
-    libpq-dev \
-    exuberant-ctags \
-    openvpn \
-    resolvconf \
-    nodejs \
-    npm \
-    most \
-    fzf \
-    ripgrep \
-    ncdu \
-    jq \
-    rar \
-    p7zip-full \
-    kubectx \
-    expect
+  build-essential \
+  cmake \
+  gawk \
+  git \
+  hub \
+  mercurial \
+  curl \
+  wget \
+  vim \
+  fish \
+  tmux \
+  rbenv \
+  libffi-dev \
+  libyaml-dev \
+  libpq-dev \
+  exuberant-ctags \
+  openvpn \
+  resolvconf \
+  nodejs \
+  npm \
+  most \
+  fzf \
+  ripgrep \
+  ncdu \
+  jq \
+  rar \
+  p7zip-full \
+  kubectx \
+  expect
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 
