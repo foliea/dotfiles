d80e5922ff2d1923fb03c4afc425e7d7497a44b3
Adrien Folie <folie.adrien@gmail.com>
2025-05-31 03:49:45 +0200

    fix: tmux tpm location

--- tmux/install.sh
+++ tmux/install.sh
@@ -10,7 +10,7 @@ mkdir -p $plugins_dir
 tpm_dir="$plugins_dir/tpm"
 
 if [ ! -d $tpm_dir ]; then
-  git clone https://github.com/tmux-plugins/tpm $tpm_dir/tpm
+  git clone https://github.com/tmux-plugins/tpm $tpm_dir
 fi
 
 # Install tmux plugins
