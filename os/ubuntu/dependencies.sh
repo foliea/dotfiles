824b58ead6c48d709a6ab3eee281c8c3ebe37df0
Adrien Folie <folie.adrien@gmail.com>
2025-06-10 20:10:03 +0200

    fix: add missing install for k9s ubuntu

--- os/ubuntu/dependencies.sh
+++ os/ubuntu/dependencies.sh
@@ -33,9 +33,15 @@ apt-get install -qy \
   kubectx \
   expect
 
+wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 
 mkdir -p "$HOME/.rbenv/plugins"
-git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build"
+if [ ! -d "$HOME/.rbenv/plugins/ruby-build" ]; then
+  git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build"
+else
+  echo "ruby-build already exists, skipping clone."
+fi
 
 curl -fsSL https://fnm.vercel.app/install | bash
