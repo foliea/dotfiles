cc8948dd5493884142e33b6018ca380324d1f5d2
Adrien Folie <folie.adrien@gmail.com>
2025-06-25 01:41:23 +0200

    fix: neovim clipboard copy paste

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -23,3 +23,24 @@ vim.opt.linebreak = true
 vim.opt.showbreak = '=>   '
 vim.opt.termguicolors = true
 vim.cmd [[colorscheme base16-tomorrow-night-eighties]]
+
+-- Clipboard integration for vim-system-copy in WSL, Wayland, X11, and VS Code
+if vim.fn.has('wsl') == 1 then
+  vim.g['system_copy#copy_command'] = 'clip.exe'
+  vim.g['system_copy#paste_command'] = 'powershell.exe -c Get-Clipboard'
+elseif vim.fn.has('macunix') == 1 then
+  vim.g['system_copy#copy_command'] = 'pbcopy'
+  vim.g['system_copy#paste_command'] = 'pbpaste'
+elseif vim.g['vscode'] ~= nil then
+  vim.g['system_copy#copy_command'] = ''
+  vim.g['system_copy#paste_command'] = ''
+elseif vim.fn.executable('wl-copy') == 1 then
+  vim.g['system_copy#copy_command'] = 'wl-copy'
+  vim.g['system_copy#paste_command'] = 'wl-paste'
+elseif vim.fn.executable('xclip') == 1 then
+  vim.g['system_copy#copy_command'] = 'xclip -selection clipboard'
+  vim.g['system_copy#paste_command'] = 'xclip -selection clipboard -o'
+elseif vim.fn.executable('xsel') == 1 then
+  vim.g['system_copy#copy_command'] = 'xsel --clipboard --input'
+  vim.g['system_copy#paste_command'] = 'xsel --clipboard --output'
+end
