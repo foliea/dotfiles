be01e8d4d2c62073f4ca04be353ec6eb68b97076
Adrien Folie <folie.adrien@gmail.com>
2025-06-27 11:20:18 +0200

    chore: remove btop.conf from source control

--- tuis/install.sh
+++ tuis/install.sh
@@ -3,7 +3,6 @@ set -e
 
 mkdir -p $HOME/.config/btop/themes
 
-ln -sf "$PWD/tuis/btop/btop.conf" $HOME/.config/btop/btop.conf
 ln -sf "$PWD/tuis/btop/themes/tomorrow-night-eighties.theme" $HOME/.config/btop/themes/tomorrow-night-eighties.theme
 
 OS="$(uname -s)"
