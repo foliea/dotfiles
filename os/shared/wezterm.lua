c656afb2c32c387449da83d14681d5506ca7fed7
Adrien Folie <folie.adrien@gmail.com>
2025-06-30 00:40:02 +0200

    chore: set font and startup for wezterm wsl

--- os/shared/wezterm.lua
+++ os/shared/wezterm.lua
@@ -5,4 +5,9 @@ local config = wezterm.config_builder()
 config.font_size = 13
 config.color_scheme = 'Tomorrow Night Eighties'
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" or wezterm.target_triple == "aarch64-pc-windows-msvc" then
+    config.default_domain = 'WSL:Ubuntu'
+    config.font_size = 10
+end
+
 return config
