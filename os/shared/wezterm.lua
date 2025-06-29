b4edf062ff5d701915b2ebb34c71f8505abf1e9f
Adrien Folie <folie.adrien@gmail.com>
2025-06-30 00:36:36 +0200

    chore: decrease wezterm font size

--- os/shared/wezterm.lua
+++ os/shared/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font_size = 14
+config.font_size = 13
 config.color_scheme = 'Tomorrow Night Eighties'
 
 return config
