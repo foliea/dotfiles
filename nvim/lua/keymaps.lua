d9b83e0de66630eb60f1075a0342b91588a3d142
Adrien Folie <folie.adrien@gmail.com>
2025-06-29 23:43:21 +0200

    chore: improve neovim keybindings

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,40 +1,34 @@
 -- Key mappings
-vim.api.nvim_set_keymap('n', '<leader>so', '<cmd>SymbolsOutline<CR>', { noremap = true })
-
+vim.api.nvim_set_keymap('', '<Up>', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('', '<Down>', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('', '<Left>', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('', '<Right>', '<Nop>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
 
+vim.g.EasyMotion_leader_key = ';'
+
 vim.api.nvim_set_keymap('n', '<C-P>', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-F>', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-I>', [[<cmd>lua require('CopilotChat').toggle()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>s', [[<cmd>lua require('spectre').open()<CR>]], { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<leader><C-P>', ':Telescope commands<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>t', ':TestNearest<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>f', ':TestFile<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>l', ':TestLast<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>v', ':TestVisit<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>c', ':Tmux ""<Left>', { noremap = true })
-
-vim.api.nvim_set_keymap('', '<Up>', '<Nop>', { noremap = true })
-vim.api.nvim_set_keymap('', '<Down>', '<Nop>', { noremap = true })
-vim.api.nvim_set_keymap('', '<Left>', '<Nop>', { noremap = true })
-vim.api.nvim_set_keymap('', '<Right>', '<Nop>', { noremap = true })
-
+vim.api.nvim_set_keymap('n', '<leader>so', ':SymbolsOutline<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tq', ":TodoTelescope<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>xx', ":Trouble<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>lg', ":LazyGit<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><C-P>', ":Telescope commands<CR>", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('n', '<C-i>', "<cmd>lua require('CopilotChat').toggle()<CR>", { noremap = true, silent = true })
-
-vim.g.EasyMotion_leader_key = ';'
+vim.api.nvim_set_keymap('n', '<leader>tq', ':TodoTelescope<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tb', ':Trouble<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lg', ':LazyGit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>df', ':DiffviewOpen<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dc', ':DiffviewClose<CR>', { noremap = true, silent = true })
 
 -- Native LSP keymaps
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>df', '<cmd>DiffviewOpen<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { noremap = true, silent = true })
