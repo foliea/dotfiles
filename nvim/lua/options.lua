90ff411c90ab9ab809e6352c2f198d3875c68a6b
Adrien Folie <folie.adrien@gmail.com>
2025-06-30 19:51:47 +0200

    fix: diagnostics deprecation

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -57,17 +57,16 @@ end
 vim.opt.clipboard:append('unnamedplus')
 
 vim.diagnostic.config({
-  signs = true,
   virtual_text = true,
   underline = true,
   update_in_insert = false,
   severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+    },
+  },
 })
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
