1de8ba3b206fec115d828ba79cf2e28d5f2c5399
Adrien Folie <folie.adrien@gmail.com>
2025-06-30 22:02:24 +0200

    feat: change keybindings for completion selection

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -45,8 +45,8 @@ pcall(function()
     mapping = cmp.mapping.preset.insert({
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<Tab>'] = cmp.mapping.select_next_item(),
-      ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+      ['<C-N'] = cmp.mapping.select_next_item(),
+      ['<C-P'] = cmp.mapping.select_prev_item(),
     }),
     sources = cmp.config.sources({
       { name = "copilot" },
