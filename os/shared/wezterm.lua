9729e361b3b43474253eddab01225227b8ae691e
Adrien Folie <folie.adrien@gmail.com>
2025-07-02 00:19:49 +0200

    feat: hide wezterm tab bar

--- os/shared/wezterm.lua
+++ os/shared/wezterm.lua
@@ -3,6 +3,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font_size = 13
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = 'NeverPrompt'
+
 -- Works better than the built-in Tomorrow Night Eighties color scheme
 config.colors = {
     -- The 16 basic ANSI colors
