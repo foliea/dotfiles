dc2d85da22c740a732681ccb227c693d0b762fab
Adrien Folie <folie.adrien@gmail.com>
2025-07-02 12:12:56 +0200

    feat: improve notifications colors (neovim)

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -8,6 +8,20 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2d2d2d" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#2d2d2d" })
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#66cccc"})
 
+-- Notification
+vim.api.nvim_set_hl(0, "NotifyERRORBorder", { fg = "#f2777a" })
+vim.api.nvim_set_hl(0, "NotifyWARNBorder", { fg = "#f99157" })
+vim.api.nvim_set_hl(0, "NotifyHINTBorder", { fg = "#ffcc66" })
+vim.api.nvim_set_hl(0, "NotifyINFOBorder", { fg = "#99cc99" })
+vim.api.nvim_set_hl(0, "NotifyERRORIcon", { fg = "#f2777a" })
+vim.api.nvim_set_hl(0, "NotifyWARNTIcon", { fg = "#f99157" })
+vim.api.nvim_set_hl(0, "NotifyHINTIcon", { fg = "#ffcc66" })
+vim.api.nvim_set_hl(0, "NotifyINFOIcon", { fg = "#99cc99" })
+vim.api.nvim_set_hl(0, "NotifyERRORTitle", { fg = "#f2777a" })
+vim.api.nvim_set_hl(0, "NotifyWARNTitle", { fg = "#f99157" })
+vim.api.nvim_set_hl(0, "NotifyHINTTitle", { fg = "#ffcc66" })
+vim.api.nvim_set_hl(0, "NotifyINFOTitle", { fg = "#99cc99" })
+
 -- Code editor
 vim.api.nvim_set_hl(0, "Delimiter", { fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "@string.special", { fg = "#f99157" })
