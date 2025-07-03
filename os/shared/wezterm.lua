fd2d80d29541cddd07cf7b7cd5621c193f63e758
Adrien Folie <folie.adrien@gmail.com>
2025-07-03 22:38:46 +0200

    chore: use fira code font explicitly (wezterm)

--- os/shared/wezterm.lua
+++ os/shared/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font "FiraCode Nerd Font"
 config.font_size = 13
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
