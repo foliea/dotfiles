27d2f9eadf66638941b7a23f9a6d3a46242d9d0f
Adrien Folie <folie.adrien@gmail.com>
2025-07-07 18:45:23 +0200

    feat: save a few macOs settings

--- os/macOS/configure.sh
+++ os/macOS/configure.sh
@@ -6,3 +6,18 @@ sh "$(dirname "$0")/../shared/configure.sh"
 # Aerospace
 mkdir -p "$HOME/.config/aerospace"
 ln -sf "$PWD/os/macOs/aerospace.toml" "$HOME/.config/aerospace/aerospace.toml"
+
+# Global macOS settings
+defaults write -g NSWindowShouldDragOnGesture -bool true
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock orientation -string left
+
+# Disable Stage Manager
+defaults write com.apple.WindowManager GloballyEnabled -bool false
+
+# Hide Desktop Widgets
+defaults write com.apple.WindowManager StandardHideWidgets -int 0
+defaults write com.apple.WindowManager StageManagerHideWidgets -int 0
+
+killall Dock
+killall WindowManager
