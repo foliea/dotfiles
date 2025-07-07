05b75febb35cb8e307dfb34eef0c0f68033d1cec
Adrien Folie <folie.adrien@gmail.com>
2025-07-08 01:16:54 +0200

    chore: configure mise with a configuration file

--- os/shared/dependencies.sh
+++ os/shared/dependencies.sh
@@ -57,16 +57,15 @@ brew install \
 # Starship
 ln -sf "$PWD/os/shared/starship.toml" "$HOME/.config/starship.toml"
 
+# Mise
+mkdir -p "$HOME/.config/mise"
+ln -sf "$PWD/os/shared/mise.toml" "$HOME/.config/mise/config.toml"
+
 # Ruby
 ln -sf "$PWD/os/shared/default-gems" "$HOME/.default-gems"
-mise settings add idiomatic_version_file_enable_tools ruby
-mise use -g ruby@latest
 
 # Node.js
 ln -sf "$PWD/os/shared/default-npm-packages" "$HOME/.default-npm-packages"
-mise settings add idiomatic_version_file_enable_tools node
-mise use -g node@latest
 
-# Terraform
-mise settings add idiomatic_version_file_enable_tools terraform
-mise use -g terraform@latest
+# Install mise tools
+mise install
