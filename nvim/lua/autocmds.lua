59779c9ef7659e6c4f4c17d305d246b383ec38a6
Adrien Folie <folie.adrien@gmail.com>
2025-07-08 10:40:56 +0200

    fix: lua tabs (use hardtab 8)

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -41,12 +41,24 @@ vim.api.nvim_create_autocmd("FileType", {
 		"sh",
 		"bash",
 		"make",
-		"lua",
 	},
 	callback = function()
 		vim.opt_local.tabstop = 4
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.softtabstop = 4
+		vim.opt_local.expandtab = false
+	end,
+})
+
+-- Hardtab 8
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"lua",
+	},
+	callback = function()
+		vim.opt_local.tabstop = 8
+		vim.opt_local.shiftwidth = 8
+		vim.opt_local.softtabstop = 8
 		vim.opt_local.expandtab = false
 	end,
 })
