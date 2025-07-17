cbadce14751bc945d4c62bbdd5dc67b513a895f9
Adrien Folie <folie.adrien@gmail.com>
2025-07-17 16:28:18 +0200

    fix(neovim): colors loading and treesitter race condition

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,13 +1,8 @@
-local uv = vim.loop
-
 local function read_json(path)
-	local fd = assert(uv.fs_open(path, "r", 438))
-	local stat = assert(uv.fs_fstat(fd))
-	local data = assert(uv.fs_read(fd, stat.size, 0))
-	uv.fs_close(fd)
-	return vim.fn.json_decode(data)
+  local lines = vim.fn.readfile(path)
+  local data = table.concat(lines, "\n")
+  return vim.fn.json_decode(data)
 end
 
 local colors = read_json(vim.fn.expand("~/.config/omamad/themes/default/neovim.json"))
-
 return colors
