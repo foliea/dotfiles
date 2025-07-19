81293b6ff9ca220f7ff42ccfe73637e248c18e03
Adrien Folie <folie.adrien@gmail.com>
2025-07-19 09:17:24 +0200

    chore(themes): improve git delta

--- themes/black-hole/git-delta.sh
+++ themes/black-hole/git-delta.sh
@@ -3,9 +3,12 @@ set -e
 
 config="git config --global"
 
-${config} delta.plus-style "syntax #141414"
-${config} delta.minus-style "syntax #141414"
-${config} delta.line-numbers-left-style "#e06c75"
-${config} delta.line-numbers-right-style "#7fd88f"
-${config} delta.line-numbers-minus-style "#e06c75"
-${config} delta.line-numbers-plus-style "#7fd88f"
+${config} delta.plus-style "syntax #1b2b34"
+${config} delta.minus-style "syntax #2d1f26"
+${config} delta.line-numbers-zero-style "#808080"
+${config} delta.line-numbers-left-style "#c53b53"
+${config} delta.line-numbers-right-style "#4fd6b3"
+${config} delta.line-numbers-minus-style "#c53b53"
+${config} delta.line-numbers-plus-style "#4fd6b3"
+${config} delta.minus-emph-style "normal #e26a75"
+${config} delta.plus-emph-style "normal #b8db87"
