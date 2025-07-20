19b6a12702956ff110b1e478397f2d5fb16ba928
Adrien Folie <folie.adrien@gmail.com>
2025-07-20 18:40:48 +0200

    chore(neovim): fix lua issues

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -1,7 +1,7 @@
 local function read_json(path)
-  local lines = vim.fn.readfile(path)
-  local data = table.concat(lines, "\n")
-  return vim.fn.json_decode(data)
+	local lines = vim.fn.readfile(path)
+	local data = table.concat(lines, "\n")
+	return vim.fn.json_decode(data)
 end
 
 local colors = read_json(vim.fn.expand("~/.config/themes/default/neovim.json"))
