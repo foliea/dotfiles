d81e4762a096a5a86af25e16f1e28984f406877f
Adrien Folie <folie.adrien@gmail.com>
2025-07-28 12:38:11 +0200

    fix(neovim): unwanted notification

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -129,32 +129,35 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		config = function()
-		require("noice").setup({
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						kind = "",
+			require("noice").setup({
+				routes = {
+					{
+						filter = {
+							any = {
+								{ event = "msg_show", find = ":qa" },
+								{ event = "notify", find = ":qa" },
+							},
+						},
+						opts = { skip = true },
 					},
-					opts = { skip = true },
 				},
-			},
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
 				},
-			},
-			presets = {
-				bottom_search = false, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
-		})		end,
+				presets = {
+					bottom_search = false, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"rcarriga/nvim-notify",
