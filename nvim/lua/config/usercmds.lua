6dd2d47ca0e42dbd070d07c39cdd535d51a59e16
Adrien Folie <folie.adrien@gmail.com>
2025-08-06 11:28:09 +0200

    chore(neovim): add tabs list cmd

--- nvim/lua/config/usercmds.lua
+++ nvim/lua/config/usercmds.lua
@@ -7,7 +7,6 @@ function Open(target)
 end
 _G.Open = Open
 
-
 function RenameFile()
 	local old_name = vim.fn.expand("%")
 	local new_name = vim.fn.input("New file name: ", old_name, "file")
@@ -32,3 +31,49 @@ function MakeExecutable()
 end
 vim.api.nvim_create_user_command("ME", MakeExecutable, {})
 
+-- Custom Telescope picker for bufferline ordinals
+local has_telescope, pickers = pcall(require, "telescope.pickers")
+if has_telescope then
+	local finders = require("telescope.finders")
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+	local conf = require("telescope.config").values
+
+	local function bufferline_ordinals_picker()
+		local bufferline = require("bufferline.state")
+		local entries = {}
+		for i, buf in ipairs(bufferline.components) do
+			local name = vim.fn.fnamemodify(vim.fn.bufname(buf.id), ":t")
+			table.insert(entries, { ordinal = i, name = name, id = buf.id })
+		end
+
+		pickers
+			.new({}, {
+				prompt_title = "Buffer Ordinals",
+				finder = finders.new_table({
+					results = entries,
+					entry_maker = function(entry)
+						return {
+							value = entry,
+							display = string.format("%d: %s", entry.ordinal, entry.name),
+							ordinal = tostring(entry.ordinal) .. " " .. entry.name,
+						}
+					end,
+				}),
+				sorter = conf.generic_sorter({}),
+				attach_mappings = function(prompt_bufnr, _)
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local selection = action_state.get_selected_entry()
+						if selection and selection.value and selection.value.ordinal then
+							vim.cmd("BufferLineGoToBuffer " .. selection.value.ordinal)
+						end
+					end)
+					return true
+				end,
+			})
+			:find()
+	end
+
+	vim.api.nvim_create_user_command("TabsList", bufferline_ordinals_picker, {})
+end
