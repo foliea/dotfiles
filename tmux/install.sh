92bd9c0cdd35f7151c81405dd888b164c79093af
Adrien Folie <folie.adrien@gmail.com>
2026-04-18 18:10:09 +0200

    refactor: remove tpm from tmux install script

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,13 +4,3 @@ set -e
 mkdir -p "$HOME/.config/tmux"
 rm -rf "$HOME/.config/tmux"/*
 cp -r "$PWD/tmux"/* "$HOME/.config/tmux/"
-
-PLUGINS_DIR="$HOME/.config/tmux/plugins"
-mkdir -p "$PLUGINS_DIR"
-
-TPM_DIR="$PLUGINS_DIR/tpm"
-if [ ! -d $TPM_DIR ]; then
-	git clone https://github.com/tmux-plugins/tpm $TPM_DIR
-fi
-
-$TPM_DIR/bin/install_plugins
