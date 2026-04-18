491758ef4b39d2e2fc7241ad633cc0b9a51c4e13
Adrien Folie <folie.adrien@gmail.com>
2026-04-18 18:40:56 +0200

    refactor: simplify git install - copy files to ~/.config/git

--- git/install.sh
+++ git/install.sh
@@ -1,52 +1,17 @@
 #!/bin/sh
 set -e
 
-config="git config --global"
+rm -rf "$HOME/.config/git"/*
+mkdir -p "$HOME/.config/git"
+cp -r "$PWD/git"/* "$HOME/.config/git/"
 
-for info in name email; do
-	read -p "Please specify you git user $info: " userinfo
-	if [ "$userinfo" ]; then
-		${config} user.$info "$userinfo"
-	fi
-done
+read -p "Please specify your git user name: " gitname
+read -p "Please specify your git user email: " gitemail
 
-read -p "Do you want to sign git commits with gpg key? (y/n): " gpgsign
-if [ "$gpgsign" = "y" ]; then
-	${config} commit.gpgsign true
+if [ "$gitname" ]; then
+	git config --global user.name "$gitname"
 fi
 
-${config} core.autocrlf input
-${config} push.default current
-${config} pull.rebase true
-${config} rerere.enabled true
-${config} color.ui true
-${config} core.excludesfile ~/.gitignore-global
-${config} protocol.version 1
-
-# Performance optimizations for faster git operations (especially in WSL)
-# - fsmonitor/untrackedcache: Improve status/add performance
-# - fscache: Cache filesystem operations
-# - preloadindex: Load index in memory on startup
-# - index.version 4: Use latest index format
-# - manyFiles: Optimize for repositories with many files
-${config} core.fsmonitor true
-${config} core.untrackedcache true
-${config} core.fscache true
-${config} core.preloadindex true
-${config} index.version 4
-${config} feature.manyFiles true
-
-# Aliases
-${config} alias.s "status -s"
-${config} alias.lg "log --oneline --decorate --all --graph"
-${config} alias.pushall "push --recurse-submodules=on-demand"
-
-# Pager
-${config} core.pager delta
-${config} interactive.diffFilter 'delta --color-only'
-${config} delta.navigate true
-${config} delta.line-numbers true
-${config} delta.side-by-side true
-${config} merge.conflictStyle zdiff3
-
-ln -sf $PWD/git/gitignore $HOME/.gitignore-global
+if [ "$gitemail" ]; then
+	git config --global user.email "$gitemail"
+fi
