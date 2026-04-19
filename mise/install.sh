c9a563eb1e2dc2b57f03cd676828882ffd375500
Adrien Folie <folie.adrien@gmail.com>
2026-04-20 01:56:42 +0200

    fix: silence mise trust, fix brew PATH in CI

--- mise/install.sh
+++ mise/install.sh
@@ -4,4 +4,4 @@ set -e
 rm -rf "$HOME/.config/mise"
 cp -r "$PWD/mise" "$HOME/.config/mise"
 
-mise trust "$HOME/.config/mise/config.toml"
+mise trust "$HOME/.config/mise/config.toml" > /dev/null 2>&1
