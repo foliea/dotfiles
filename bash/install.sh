e5f840d27d3069077e35e25005b83aa2f7d2d354
Adrien Folie <folie.adrien@gmail.com>
2026-04-20 00:27:58 +0200

    move bash brew file as env

--- bash/install.sh
+++ bash/install.sh
@@ -6,7 +6,7 @@ cp "$PWD/bash/bash_profile" "$HOME/.bash_profile"
 
 rm -rf "$HOME/.config/bash"
 mkdir -p "$HOME/.config/bash"
-cp "$PWD/bash/brew" "$HOME/.config/bash/brew"
+cp "$PWD/bash/env" "$HOME/.config/bash/env"
 cp "$PWD/bash/functions" "$HOME/.config/bash/functions"
 cp "$PWD/bash/aliases" "$HOME/.config/bash/aliases"
 
