10f5bbbdc7d789041ba7523c2082c46dd2d6efdc
Adrien Folie <folie.adrien@gmail.com>
2026-04-19 17:26:10 +0200

    extract bash aliases

--- bash/install.sh
+++ bash/install.sh
@@ -3,8 +3,12 @@ set -e
 
 cp "$PWD/bash/bashrc" "$HOME/.bashrc"
 cp "$PWD/bash/bash_profile" "$HOME/.bash_profile"
+
+rm -rf "$HOME/.config/bash"
 mkdir -p "$HOME/.config/bash"
 cp "$PWD/bash/brew" "$HOME/.config/bash/brew"
 cp "$PWD/bash/functions" "$HOME/.config/bash/functions"
+cp "$PWD/bash/aliases" "$HOME/.config/bash/aliases"
+
 mkdir -p "$HOME/.config/bash/fns"
 cp -r "$PWD/bash/fns"/* "$HOME/.config/bash/fns/"
