63269bb1a00882a2e3beefe2492d37b2deec1695
Adrien Folie <folie.adrien@gmail.com>
2026-04-21 00:24:25 +0200

    omarchy: add audio fix script for Zenbook S 14 UX5406SA

---
+++ os/omarchy/scripts/fix_audio_ux5406sa.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+# Audio Fix for Zenbook S 14 UX5406SA (Lunar Lake + ALC285)
+
+sudo pacman -S --noconfirm \
+  sof-firmware \
+  alsa-ucm-conf \
+  alsa-utils \
+  pipewire-alsa \
+  pipewire-pulse \
+  wireplumber
+
+echo "options snd-intel-dspcfg dsp_driver=3" | sudo tee /etc/modprobe.d/sof.conf
+echo "options snd-hda-intel model=alc285-zenbook" | sudo tee /etc/modprobe.d/alsa-base.conf
+
+mkdir -p $HOME/.config/pipewire/pipewire.conf.d/
+cat <<EOF > $HOME/.config/pipewire/pipewire.conf.d/zenbook-hifi.conf
+stream.properties = {
+    channelmix.upmix = true
+    channelmix.upmix-method = psd
+    channelmix.lfe-cutoff = 150
+}
+EOF
+
+rm -rf $HOME/.config/pulse $HOME/.local/state/wireplumber $HOME/.config/chromium/Default/AudioState 2>/dev/null
+
+echo "Done. Reboot required."
