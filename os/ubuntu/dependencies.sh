0eb451d914be361820e9550841e33e85067ed8dc
Adrien Folie <folie.adrien@gmail.com>
2026-04-20 10:45:58 +0200

    fix ubuntu deps script

--- os/ubuntu/dependencies.sh
+++ os/ubuntu/dependencies.sh
@@ -2,42 +2,42 @@
 set -e
 
 install_ubuntu_deps() {
-	apt-get update
-	apt-get install -y \
-		build-essential \
-		procps \
-		curl \
-		file \
-		git \
-		python3 \
-		sudo \
-		locales \
-		libffi-dev \
-		libyaml-dev \
-		zlib1g-dev
-	locale-gen en_US.UTF-8
+  apt-get update
+  apt-get install -y \
+    build-essential \
+    procps \
+    curl \
+    file \
+    git \
+    python3 \
+    sudo \
+    locales \
+    libffi-dev \
+    libyaml-dev \
+    zlib1g-dev
+  locale-gen en_US.UTF-8
 }
 
 install_homebrew() {
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 install_shared_deps() {
-	./os/shared/dependencies.sh
+  sh "$(dirname "$0")/../shared/dependencies.sh"
 }
 
 main() {
-	sudo install_ubuntu_deps
-	install_homebrew
+  sudo install_ubuntu_deps
+  install_homebrew
   install_shared_deps
 }
 
 if [ "$1" = "install_ubuntu_deps" ]; then
-	install_ubuntu_deps
+  install_ubuntu_deps
 elif [ "$1" = "install_homebrew" ]; then
-	install_homebrew
+  install_homebrew
 elif [ "$1" = "install_shared_deps" ]; then
-	install_shared_deps
+  install_shared_deps
 else
-	main "$@"
+  main "$@"
 fi
