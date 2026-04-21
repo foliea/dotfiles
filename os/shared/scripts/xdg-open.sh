8f9698f1eed267eaca9a36e51edcd46df4e3b3d1
Adrien Folie <folie.adrien@gmail.com>
2026-04-21 11:39:42 +0200

    refactor xdg-open wrapper

--- os/shared/scripts/xdg-open.sh
+++ os/shared/scripts/xdg-open.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # xdg-open wrapper to use Windows/macOs host applications
 
-# Check if running in WSL
-if [ ! -f /proc/version ] || ! grep -qi microsoft /proc/version; then
-  exec /usr/bin/xdg-open "$@"
-fi
-
 # Check if running on macOs
 if [ "$(uname)" = "Darwin" ]; then
   exec open "$@"
 fi
 
-# Check if cmd.exe is available
+# Check if not running in WSL
+if [ ! -f /proc/version ] || ! grep -qi microsoft /proc/version; then
+  exec /usr/bin/xdg-open "$@"
+fi
+
+# Check Windows interop is available
 if ! command -v cmd.exe >/dev/null 2>&1; then
   echo "Error: cmd.exe not found. Make sure Windows interop is enabled" >&2
   exit 1
