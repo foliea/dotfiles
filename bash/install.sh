edd0e5a55ebebee12a55cac9ed4b3e51c7ac5763
Adrien Folie <folie.adrien@gmail.com>
2026-04-22 17:19:13 +0200

    fix bash install script copying files

--- bash/install.sh
+++ bash/install.sh
@@ -5,9 +5,10 @@ rm -rf "$HOME/.config/bash"
 mkdir -p "$HOME/.config/bash/fns"
 
 for file in env functions aliases; do
-	cp "$PWD/bash/$file" "$HOME/.config/bash/$file"
+  cp "$PWD/bash/$file" "$HOME/.config/bash/$file"
 done
 cp -r "$PWD/bash/fns"/* "$HOME/.config/bash/fns/"
 
+rm -f "$HOME/.bashrc" "$HOME/.bash_profile"
 cp "$PWD/bash/bashrc" "$HOME/.bashrc"
 cp "$PWD/bash/bash_profile" "$HOME/.bash_profile"
