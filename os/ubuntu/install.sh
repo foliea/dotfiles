b0a243b78c0105dd562931440205caf72e244c06
Adrien Folie <folie.adrien@gmail.com>
2026-04-23 17:48:06 +0200

    ubuntu: install chromium, mpv, discord, caffeine

--- os/ubuntu/install.sh
+++ os/ubuntu/install.sh
@@ -3,5 +3,19 @@ set -e
 
 sh "$(dirname "$0")/../shared/install.sh"
 
+# Discord (via community APT repo)
+curl -sSL https://palfrey.github.io/discord-apt/discord-repo_1.2_all.deb -o /tmp/discord-repo.deb
+sudo dpkg -i /tmp/discord-repo.deb
+rm /tmp/discord-repo.deb
+
+# Packages
+sudo apt update
+
+sudo apt install -y \
+  chromium \
+  mpv \
+  caffeine \
+  discord
+
 # Ghostty
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
