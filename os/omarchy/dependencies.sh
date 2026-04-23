2c65d34dc8857ed70a87eb1593224aa4c470a755
Adrien Folie <folie.adrien@gmail.com>
2026-04-23 09:26:31 +0200

    make omarchy deps scripts also use shared scripts

--- os/omarchy/dependencies.sh
+++ os/omarchy/dependencies.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -e
 
+sh "$(dirname "$0")/../shared/dependencies.sh"
+
 omarchy-pkg-add \
   "FiraCode Nerd Font" \
   yazi \
   git-delta \
   makima-bin \
   k9s
-
-for script in xdg-open pkg-install-menu open-folder-menu; do
-  cp "$PWD/os/shared/scripts/$script.sh" "$HOME/.local/bin/$script"
-  chmod +x "$HOME/.local/bin/$script"
-done
--- os/shared/dependencies.sh
+++ os/shared/dependencies.sh
@@ -1,57 +1,59 @@
 #!/bin/sh
 set -e
 
-brew update
+if ! command -v omarchy-version >/dev/null 2>&1; then
+  brew update
 
-brew install \
-  wget \
-  curl \
-  cmake \
-  gawk \
-  readline \
-  bat \
-  expect \
-  jq \
-  fzf \
-  ripgrep \
-  fd \
-  eza \
-  zoxide \
-  git \
-  git-delta \
-  gh \
-  bash \
-  starship \
-  yazi \
-  tmux \
-  neovim \
-  mise \
-  ncdu \
-  kubectx \
-  kubeseal \
-  helm \
-  awscli \
-  fastfetch \
-  btop \
-  k9s \
-  ffmpeg \
-  sevenzip \
-  poppler \
-  resvg \
-  imagemagick \
-  lazygit \
-  jesseduffield/lazydocker/lazydocker \
-  anomalyco/tap/opencode \
-  Valkyrie00/homebrew-bbrew/bbrew \
-  stylua \
-  prettier \
-  prettierd \
-  shfmt \
-  beautysh \
-  lua-language-server \
-  bash-language-server \
-  dockerfile-language-server \
-  vscode-langservers-extracted
+  brew install \
+    wget \
+    curl \
+    cmake \
+    gawk \
+    readline \
+    bat \
+    expect \
+    jq \
+    fzf \
+    ripgrep \
+    fd \
+    eza \
+    zoxide \
+    git \
+    git-delta \
+    gh \
+    bash \
+    starship \
+    yazi \
+    tmux \
+    neovim \
+    mise \
+    ncdu \
+    kubectx \
+    kubeseal \
+    helm \
+    awscli \
+    fastfetch \
+    btop \
+    k9s \
+    ffmpeg \
+    sevenzip \
+    poppler \
+    resvg \
+    imagemagick \
+    lazygit \
+    jesseduffield/lazydocker/lazydocker \
+    anomalyco/tap/opencode \
+    Valkyrie00/homebrew-bbrew/bbrew \
+    stylua \
+    prettier \
+    prettierd \
+    shfmt \
+    beautysh \
+    lua-language-server \
+    bash-language-server \
+    dockerfile-language-server \
+    vscode-langservers-extracted
+fi
 
 # Local bin scripts
 mkdir -p "$HOME/.local/bin"
