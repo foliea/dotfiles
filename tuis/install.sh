298cfe69b829c71fbd00f49bb766b2916e8aaef9
Adrien Folie <folie.adrien@gmail.com>
2026-04-23 11:24:22 +0200

    simplify tuis install script again

--- tuis/install.sh
+++ tuis/install.sh
@@ -1,30 +1,27 @@
 #!/bin/sh
 set -e
 
-OS="$(uname -s)"
-if [ "$OS" = "Darwin" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
   CONFIG_DIR="$HOME/Library/Application Support"
-elif [ "$OS" = "Linux" ]; then
-  CONFIG_DIR="$HOME/.config"
 else
-  echo "Unsupported OS: $OS"
-  exit 1
+  CONFIG_DIR="$HOME/.config"
 fi
 
-install_tools() (
-  for tool in "$1"; do
-    local tool_config_dir = "$2/$tool"
+install_tuis() (
+  for tui in $1; do
+    tui_config_dir="$2/$tui"
 
-    echo $tool $tool_config_dir
-    #    mkdir -p $tool_config_dir
-    #rm -rf $tool_config_dir/*
-    #  cp -r "$PWD/tuis/$tool"/* "$tool_config_dir/"
+    mkdir -p "$tui_config_dir"
+    rm -rf "$tui_config_dir"/*
+    cp -r "$PWD/tuis/$tui"/* "$tui_config_dir/"
   done
 )
 
-install_tools "k9s lazygit lazydocker" "$CONFIG_DIR"
-install_tools "yazi opencode" "$HOME/.config"
+install_tuis "k9s lazygit lazydocker" "$CONFIG_DIR"
+
+install_tuis "yazi opencode" "$HOME/.config"
 
 if ! command -v omarchy-version >/dev/null 2>&1; then
-  install_tools "fastfetch" "$HOME/.config"
+  install_tuis "fastfetch" "$HOME/.config/fastfetch"
 fi
+
